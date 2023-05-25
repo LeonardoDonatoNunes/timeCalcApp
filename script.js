95b23a00b8110a14c167af5e49851641539d8b38
leodnunes <l.donato@gestaoagropecuaria.com.br>
2023-05-25 09:52:12 -0300

    Adição do ícone e alteração de labels

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function adicionarCampos() {
     novaLinha.innerHTML = `
         <td>
             <div class="form-group">
-                <label for="entrada">Entrada:</label>
+                <label for="entrada">Entrada</label>
                 <input type="time" name="entrada[]" class="entrada">
             </div>
         </td>
         <td>
             <div class="form-group">
-                <label for="saida">Saída:</label>
+                <label for="saida">Saída</label>
                 <input type="time" name="saida[]" class="saida">
             </div>
         </td>
